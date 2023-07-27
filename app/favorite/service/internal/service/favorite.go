9d3ad0bc555b09d08a89d221f186ad293199a5bb
intyouss <123916296+intyouss@users.noreply.github.com>
2023-07-27 17:18:30 +0800

    refactor: Look at the big picture and reprogram the catalog

---
+++ app/favorite/service/internal/service/favorite.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"context"
+
+	pb "Atreus/api/favorite/service/v1"
+)
+
+type FavoriteService struct {
+	pb.UnimplementedFavoriteServer
+}
+
+func NewFavoriteService() *FavoriteService {
+	return &FavoriteService{}
+}
+
+func (s *FavoriteService) GetFavoriteList(ctx context.Context, req *pb.FavoriteListRequest) (*pb.FavoriteListReply, error) {
+	return &pb.FavoriteListReply{}, nil
+}
+func (s *FavoriteService) FavoriteAction(ctx context.Context, req *pb.FavoriteActionRequest) (*pb.FavoriteActionReply, error) {
+	return &pb.FavoriteActionReply{}, nil
+}
---
+++ app/relation/service/internal/service/relation.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"context"
+
+	pb "Atreus/api/relation/service/v1"
+)
+
+type RelationService struct {
+	pb.UnimplementedRelationServer
+}
+
+func NewRelationService() *RelationService {
+	return &RelationService{}
+}
+
+func (s *RelationService) GetFollowerRelationList(ctx context.Context, req *pb.RelationFollowerListRequest) (*pb.RelationFollowerListReply, error) {
+	return &pb.RelationFollowerListReply{}, nil
+}
+func (s *RelationService) GetFollowRelationList(ctx context.Context, req *pb.RelationFollowListRequest) (*pb.RelationFollowListReply, error) {
+	return &pb.RelationFollowListReply{}, nil
+}
+func (s *RelationService) RelationAction(ctx context.Context, req *pb.RelationActionRequest) (*pb.RelationActionReply, error) {
+	return &pb.RelationActionReply{}, nil
+}
