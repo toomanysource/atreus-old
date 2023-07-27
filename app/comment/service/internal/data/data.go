2ae0b98749d8fe5a58cd7bd8eb46cb24c5f3b677
intyouss <123916296+intyouss@users.noreply.github.com>
2023-07-27 19:59:23 +0800

    fix: Resolve app file error reporting issues and standardize internal file directories

--- app/comment/service/internal/data/data.go
+++ app/comment/service/internal/data/data.go
@@ -8,7 +8,7 @@ import (
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
+var ProviderSet = wire.NewSet(NewData, NewCommentRepo)
 
 // Data .
 type Data struct {
