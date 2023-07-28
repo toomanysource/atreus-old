0c436db00c24ee0d9d33186cf7f2246816367796
Alilestera <75608652+alilestera@users.noreply.github.com>
2023-07-28 09:56:28 +0800

    feat: simplify code struct, create service example to help with developing (#11)
    * mod: simplify code structure, keep important files
    
    * mod: add third_party proto file, add http option to user.proto and regenerate its *.pb.go files
    
    * mod: modify the conf.proto,  regenerate its pb.go, and update config.yaml
    
    * feat: user service example without ut

--- app/user/service/internal/service/user.go
+++ app/user/service/internal/service/user.go
@@ -1,25 +1,80 @@
 package service
 
 import (
+	"Atreus/app/user/service/internal/biz"
 	"context"
+	"github.com/go-kratos/kratos/v2/log"
 
 	pb "Atreus/api/user/service/v1"
 )
 
 type UserService struct {
 	pb.UnimplementedUserServiceServer
+	log *log.Helper
+
+	usecase *biz.UserUsecase
 }
 
-func NewUserService() *UserService {
-	return &UserService{}
+func NewUserService(uc *biz.UserUsecase, logger log.Logger) *UserService {
+	return &UserService{usecase: uc, log: log.NewHelper(logger)}
 }
 
 func (s *UserService) UserRegister(ctx context.Context, req *pb.UserRegisterRequest) (*pb.UserRegisterReply, error) {
-	return &pb.UserRegisterReply{}, nil
+	user, err := s.usecase.Register(ctx, req.Username, req.Password)
+	if err != nil {
+		return &pb.UserRegisterReply{
+			StatusCode: 300,
+			StatusMsg:  "failed",
+		}, nil
+	}
+
+	return &pb.UserRegisterReply{
+		StatusCode: 200,
+		StatusMsg:  "success",
+		UserId:     user.ID,
+	}, nil
 }
+
 func (s *UserService) UserLogin(ctx context.Context, req *pb.UserLoginRequest) (*pb.UserLoginReply, error) {
-	return &pb.UserLoginReply{}, nil
+	user, err := s.usecase.Login(ctx, req.Username, req.Password)
+	if err != nil {
+		return &pb.UserLoginReply{
+			StatusCode: 300,
+			StatusMsg:  "failed",
+		}, nil
+	}
+
+	return &pb.UserLoginReply{
+		StatusCode: 200,
+		StatusMsg:  "success",
+		UserId:     user.ID,
+	}, nil
 }
+
 func (s *UserService) GetUserInfo(ctx context.Context, req *pb.UserInfoRequest) (*pb.UserInfoReply, error) {
-	return &pb.UserInfoReply{}, nil
+	user, err := s.usecase.GetInfo(ctx, req.UserId)
+	if err != nil {
+		return &pb.UserInfoReply{
+			StatusCode: 300,
+			StatusMsg:  "failed",
+		}, nil
+	}
+
+	return &pb.UserInfoReply{
+		StatusCode: 200,
+		StatusMsg:  "success",
+		User: &pb.User{
+			Id:              user.ID,
+			Name:            user.Name,
+			FollowCount:     user.FollowCount,
+			FollowerCount:   user.FollowerCount,
+			IsFollow:        user.IsFollow,
+			Avatar:          user.Avatar,
+			BackgroundImage: user.BackgroundImage,
+			Signature:       user.Signature,
+			TotalFavorited:  user.TotalFavorited,
+			WorkCount:       user.WorkCount,
+			FavoriteCount:   user.FavoriteCount,
+		},
+	}, nil
 }
