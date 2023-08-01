8ef6f87c6ad5a2ff37c551b5cf43321d5a6967a2
intyouss <123916296+intyouss@users.noreply.github.com>
2023-08-01 23:32:14 +0800

    fix&feat: Dependency injection using wire, generating injection functions, modifying entry functions

---
+++ app/comment/service/cmd/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"Atreus/app/comment/service/internal/conf"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/config"
+	"github.com/go-kratos/kratos/v2/config/file"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	"github.com/go-kratos/kratos/v2/transport/http"
+
+	_ "go.uber.org/automaxprocs"
+)
+
+// go build -ldflags "-X main.Version=x.y.z"
+var (
+	// Name is the name of the compiled software.
+	Name = "comment.service"
+	// Version is the version of the compiled software.
+	Version = "1.0.0"
+	// flagconf is the config flag.
+	flagconf string
+)
+
+func init() {
+	flag.StringVar(&flagconf, "conf", "../configs", "config path, eg: -conf config.yaml")
+}
+
+func newApp(logger log.Logger, gs *grpc.Server, hs *http.Server) *kratos.App {
+	return kratos.New(
+		kratos.Name(Name),
+		kratos.Version(Version),
+		kratos.Metadata(map[string]string{}),
+		kratos.Logger(logger),
+		kratos.Server(
+			gs,
+			hs,
+		),
+	)
+}
+
+func main() {
+	flag.Parse()
+	logger := log.With(log.NewStdLogger(os.Stdout),
+		"service.name", Name,
+		"service.version", Version,
+		"time", log.Timestamp("2006-01-02 15:04:05"),
+		"caller", log.DefaultCaller,
+		// "service.id", id,
+		// "trace.id", tracing.TraceID(),
+		// "span.id", tracing.SpanID(),
+	)
+	c := config.New(
+		config.WithSource(
+			file.NewSource(flagconf),
+		),
+	)
+	defer c.Close()
+
+	if err := c.Load(); err != nil {
+		panic(err)
+	}
+	var bc conf.Bootstrap
+	if err := c.Scan(&bc); err != nil {
+		panic(err)
+	}
+	app, cleanup, err := wireApp(bc.Server, bc.Client, bc.Data, bc.Jwt, logger)
+	if err != nil {
+		panic(err)
+	}
+	defer cleanup()
+	// start and wait for stop signal
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
+}
