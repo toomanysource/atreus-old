68ee691919789bffea030792a0963c24e8bb525c
intyouss <123916296+intyouss@users.noreply.github.com>
2023-08-02 22:22:54 +0800

    fix:Put the JWT method into the pkg package as an external introduction, modify the logic of comment.go regarding jwt

--- app/comment/service/internal/biz/comment.go
+++ app/comment/service/internal/biz/comment.go
@@ -2,10 +2,10 @@ package biz
 
 import (
 	"Atreus/app/comment/service/internal/conf"
+	"Atreus/pkg"
 	"context"
 	"errors"
 	"github.com/go-kratos/kratos/v2/log"
-	"github.com/golang-jwt/jwt/v4"
 )
 
 type Comment struct {
@@ -46,37 +46,13 @@ func NewCommentUsecase(conf *conf.JWT, cr CommentRepo, logger log.Logger) *Comme
 	return &CommentUsecase{config: conf, commentRepo: cr, log: log.NewHelper(log.With(logger, "model", "usecase/comment"))}
 }
 
-// parseToken 接收TokenString进行校验
-func (uc *CommentUsecase) parseToken(tokenKey, tokenString string) (*jwt.Token, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return []byte(tokenKey), nil
-	})
-	if err != nil {
-		log.Errorf("Server failed to convert Token, err :", err.Error())
-		return nil, err
-	}
-	if token.Valid {
-		return token, nil
-	}
-	return nil, errors.New("invalid JWT token")
-}
-
-// getTokenData 获取Token中的用户数据,返回的是map[string]any类型，需要断言
-func (uc *CommentUsecase) getTokenData(token *jwt.Token) (map[string]any, error) {
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if ok {
-		return claims, nil
-	}
-	return nil, errors.New("failed to extract claims from JWT token")
-}
-
 func (uc *CommentUsecase) GetCommentList(
 	ctx context.Context, tokenString string, videoId uint32) ([]*Comment, error) {
-	token, err := uc.parseToken(uc.config.Http.TokenKey, tokenString)
+	token, err := pkg.ParseToken(uc.config.Http.TokenKey, tokenString)
 	if err != nil {
 		return nil, err
 	}
-	_, err = uc.getTokenData(token)
+	_, err = pkg.GetTokenData(token)
 	if err != nil {
 		return nil, err
 	}
@@ -86,11 +62,11 @@ func (uc *CommentUsecase) GetCommentList(
 func (uc *CommentUsecase) CommentAction(
 	ctx context.Context, videoId, commentId uint32,
 	actionType uint32, commentText string, tokenString string) (*Comment, error) {
-	token, err := uc.parseToken(uc.config.Http.TokenKey, tokenString)
+	token, err := pkg.ParseToken(uc.config.Http.TokenKey, tokenString)
 	if err != nil {
 		return nil, err
 	}
-	data, err := uc.getTokenData(token)
+	data, err := pkg.GetTokenData(token)
 	if err != nil {
 		return nil, err
 	}
---
+++ pkg/jwt.go
@@ -0,0 +1,31 @@
+package pkg
+
+import (
+	"errors"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+// ParseToken 接收TokenString进行校验
+func ParseToken(tokenKey, tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenKey), nil
+	})
+	if err != nil {
+		log.Errorf("Server failed to convert Token, err :", err.Error())
+		return nil, err
+	}
+	if token.Valid {
+		return token, nil
+	}
+	return nil, errors.New("invalid JWT token")
+}
+
+// GetTokenData 获取Token中的用户数据,返回的是map[string]any类型，需要断言
+func GetTokenData(token *jwt.Token) (map[string]any, error) {
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok {
+		return claims, nil
+	}
+	return nil, errors.New("failed to extract claims from JWT token")
+}
