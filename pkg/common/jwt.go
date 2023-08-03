bdc87070f458cf82740c6d701c4d15bcb490bc70
intyouss <123916296+intyouss@users.noreply.github.com>
2023-08-03 16:52:59 +0800

    fix:Fix error when userId critical data is not present in token

--- pkg/common/jwt.go
+++ pkg/common/jwt.go
@@ -24,8 +24,12 @@ func ParseToken(tokenKey, tokenString string) (*jwt.Token, error) {
 // GetTokenData 获取Token中的用户数据,返回的是map[string]any类型，需要断言
 func GetTokenData(token *jwt.Token) (map[string]any, error) {
 	claims, ok := token.Claims.(jwt.MapClaims)
-	if ok {
-		return claims, nil
+	if !ok {
+		return nil, errors.New("failed to extract claims from JWT token")
 	}
-	return nil, errors.New("failed to extract claims from JWT token")
+	_, ok = claims["user_id"]
+	if !ok {
+		return nil, errors.New("the token does not carry critical data")
+	}
+	return claims, nil
 }
