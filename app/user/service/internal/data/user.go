abbff4fbc2d1862857d096038a710b4bfadb6caa
alilestera <alilestera1404@outlook.com>
2023-08-08 21:39:43 +0800

    feat: simple user service

--- app/user/service/internal/data/user.go
+++ app/user/service/internal/data/user.go
@@ -1,34 +1,187 @@
 package data
 
 import (
-	"context"
-
 	"Atreus/app/user/service/internal/biz"
-
+	"Atreus/pkg/gorms"
+	"context"
+	"errors"
 	"github.com/go-kratos/kratos/v2/log"
+	"gorm.io/gorm"
 )
+
+var userTableName = "users"
+
+type User struct {
+	*biz.User
+	gorm.DeletedAt
+}
+
+func (User) TableName() string {
+	return userTableName
+}
 
 type userRepo struct {
 	data *Data
 	log  *log.Helper
 }
 
-// NewUserRepo .
-func NewUserRepo(data *Data, logger log.Logger) biz.UserRepo {
-	return &userRepo{
-		data: data,
-		log:  log.NewHelper(logger),
+func (*userRepo) initDB(conn *gorms.GormConn) {
+	db := conn.Session(context.Background())
+	if err := db.AutoMigrate(&User{}); err != nil {
+		log.Fatalf("Database %s initialization error, err : %s", userTableName, err.Error())
 	}
 }
 
+// NewUserRepo .
+func NewUserRepo(data *Data, logger log.Logger) biz.UserRepo {
+	repo := &userRepo{
+		data: data,
+		log:  log.NewHelper(logger),
+	}
+	conn := data.db
+	repo.initDB(conn)
+	return repo
+}
+
+// Save .
 func (r *userRepo) Save(ctx context.Context, user *biz.User) (*biz.User, error) {
+	u := User{User: user}
+	session := r.data.db.Session(ctx)
+	err := session.Save(&u).Error
+	if err != nil {
+		return nil, err
+	}
+
 	return user, nil
 }
 
-func (r *userRepo) FindByID(ctx context.Context, id int64) (*biz.User, error) {
-	return &biz.User{ID: id}, nil
+// FindById .
+func (r *userRepo) FindById(ctx context.Context, id uint32) (*biz.User, error) {
+	u := new(User)
+	session := r.data.db.Session(ctx)
+	err := session.Model(&User{}).
+		Where("id = ?", id).Limit(1).
+		Take(u).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return &biz.User{}, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return u.User, nil
 }
 
+// FindByIds .
+func (r *userRepo) FindByIds(ctx context.Context, ids []uint32) ([]*biz.User, error) {
+	var us []*User
+	session := r.data.db.Session(ctx)
+	err := session.Model(&User{}).
+		Where("id IN ?", ids).
+		Find(&us).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return []*biz.User{}, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	bizus := make([]*biz.User, len(us))
+	for i := range bizus {
+		bizus[i] = us[i].User
+	}
+	return bizus, nil
+}
+
+// FindByUsername .
 func (r *userRepo) FindByUsername(ctx context.Context, username string) (*biz.User, error) {
-	return &biz.User{Username: username}, nil
+	u := new(User)
+	session := r.data.db.Session(ctx)
+	err := session.Model(&User{}).
+		Where("username = ?", username).Limit(1).
+		Take(u).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return &biz.User{}, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return u.User, nil
+}
+
+// UpdateInfo not implement yet
+func (r *userRepo) UpdateInfo(ctx context.Context, info *biz.UserInfo) error {
+	return errors.New("not implement")
+}
+
+// UpdateFollow .
+func (r *userRepo) UpdateFollow(ctx context.Context, id uint32, followChange int32) error {
+	user, err := r.FindById(ctx, id)
+	if err != nil {
+		return err
+	}
+	modifiedValue := calculateValidUint32(user.FollowCount, followChange)
+	session := r.data.db.Session(ctx)
+	sqlStmt := "update " + userTableName + " set follow_count = ? where id = ?"
+	return session.Exec(sqlStmt, modifiedValue, id).Error
+}
+
+// UpdateFollower .
+func (r *userRepo) UpdateFollower(ctx context.Context, id uint32, followerChange int32) error {
+	user, err := r.FindById(ctx, id)
+	if err != nil {
+		return err
+	}
+	modifiedValue := calculateValidUint32(user.FollowerCount, followerChange)
+	session := r.data.db.Session(ctx)
+	sqlStmt := "update " + userTableName + " set follower_count = ? where id = ?"
+	return session.Exec(sqlStmt, modifiedValue, id).Error
+}
+
+// UpdateFavorited .
+func (r *userRepo) UpdateFavorited(ctx context.Context, id uint32, favoritedChange int32) error {
+	user, err := r.FindById(ctx, id)
+	if err != nil {
+		return err
+	}
+	modifiedValue := calculateValidUint32(user.TotalFavorited, favoritedChange)
+	session := r.data.db.Session(ctx)
+	sqlStmt := "update " + userTableName + " set total_favorited = ? where id = ?"
+	return session.Exec(sqlStmt, modifiedValue, id).Error
+}
+
+// UpdateWork .
+func (r *userRepo) UpdateWork(ctx context.Context, id uint32, workChange int32) error {
+	user, err := r.FindById(ctx, id)
+	if err != nil {
+		return err
+	}
+	modifiedValue := calculateValidUint32(user.WorkCount, workChange)
+	session := r.data.db.Session(ctx)
+	sqlStmt := "update " + userTableName + " set work_count = ? where id = ?"
+	return session.Exec(sqlStmt, modifiedValue, id).Error
+}
+
+// UpdateFavorite .
+func (r *userRepo) UpdateFavorite(ctx context.Context, id uint32, favoriteChange int32) error {
+	user, err := r.FindById(ctx, id)
+	if err != nil {
+		return err
+	}
+	modifiedValue := calculateValidUint32(user.FavoriteCount, favoriteChange)
+	session := r.data.db.Session(ctx)
+	sqlStmt := "update " + userTableName + " set favorite_count = ? where id = ?"
+	return session.Exec(sqlStmt, modifiedValue, id).Error
+}
+
+func calculateValidUint32(src uint32, mod int32) uint32 {
+	if mod < 0 {
+		mod = -mod
+		if src < uint32(mod) {
+			return 0
+		}
+		return src - uint32(mod)
+	}
+	return src + uint32(mod)
 }
--- app/user/service/internal/service/user.go
+++ app/user/service/internal/service/user.go
@@ -10,71 +10,196 @@ import (
 
 type UserService struct {
 	pb.UnimplementedUserServiceServer
+
 	log *log.Helper
 
-	usecase *biz.UserUsecase
+	uc *biz.UserUsecase
 }
 
 func NewUserService(uc *biz.UserUsecase, logger log.Logger) *UserService {
-	return &UserService{usecase: uc, log: log.NewHelper(logger)}
+	return &UserService{uc: uc, log: log.NewHelper(logger)}
 }
 
 func (s *UserService) UserRegister(ctx context.Context, req *pb.UserRegisterRequest) (*pb.UserRegisterReply, error) {
-	user, err := s.usecase.Register(ctx, req.Username, req.Password)
+	user, err := s.uc.Register(context.TODO(), req.Username, req.Password)
 	if err != nil {
 		return &pb.UserRegisterReply{
 			StatusCode: 300,
-			StatusMsg:  "failed",
+			StatusMsg:  err.Error(),
 		}, nil
 	}
-
 	return &pb.UserRegisterReply{
-		StatusCode: 200,
+		StatusCode: 0,
 		StatusMsg:  "success",
-		UserId:     user.ID,
+		UserId:     user.Id,
 	}, nil
 }
 
 func (s *UserService) UserLogin(ctx context.Context, req *pb.UserLoginRequest) (*pb.UserLoginReply, error) {
-	user, err := s.usecase.Login(ctx, req.Username, req.Password)
+	user, err := s.uc.Login(context.TODO(), req.Username, req.Password)
 	if err != nil {
 		return &pb.UserLoginReply{
 			StatusCode: 300,
-			StatusMsg:  "failed",
+			StatusMsg:  err.Error(),
 		}, nil
 	}
-
 	return &pb.UserLoginReply{
-		StatusCode: 200,
+		StatusCode: 0,
 		StatusMsg:  "success",
-		UserId:     user.ID,
+		UserId:     user.Id,
 	}, nil
 }
 
 func (s *UserService) GetUserInfo(ctx context.Context, req *pb.UserInfoRequest) (*pb.UserInfoReply, error) {
-	user, err := s.usecase.GetInfo(ctx, req.UserId)
+	user, err := s.uc.GetInfo(context.TODO(), req.UserId)
 	if err != nil {
 		return &pb.UserInfoReply{
 			StatusCode: 300,
-			StatusMsg:  "failed",
+			StatusMsg:  err.Error(),
 		}, nil
 	}
-
-	return &pb.UserInfoReply{
-		StatusCode: 200,
+	reply := &pb.UserInfoReply{
+		StatusCode: 0,
 		StatusMsg:  "success",
-		User: &pb.User{
-			Id:              user.ID,
+	}
+	// no need IsFollow
+	reply.User = &pb.User{
+		Id:              user.Id,
+		Name:            user.Name,
+		FollowCount:     user.FollowCount,
+		FollowerCount:   user.FollowerCount,
+		Avatar:          user.Avatar,
+		BackgroundImage: user.BackgroundImage,
+		Signature:       user.Signature,
+		TotalFavorited:  user.TotalFavorited,
+		WorkCount:       user.WorkCount,
+		FavoriteCount:   user.FavoriteCount,
+	}
+	return reply, nil
+}
+
+func (s *UserService) UpdateUserInfo(ctx context.Context, req *pb.UpdateUserInfoRequest) (*pb.UpdateUserInfoReply, error) {
+	info := &biz.UserInfo{
+		Name:            req.Name,
+		Avatar:          req.Avatar,
+		BackgroundImage: req.BackgroundImage,
+		Signature:       req.Signature,
+	}
+
+	err := s.uc.UpdateInfo(context.TODO(), info)
+	if err != nil {
+		return &pb.UpdateUserInfoReply{
+			StatusCode: 300,
+			StatusMsg:  err.Error(),
+		}, nil
+	}
+	return &pb.UpdateUserInfoReply{
+		StatusCode: 0,
+		StatusMsg:  "success",
+	}, nil
+}
+
+func (s *UserService) GetUserInfos(ctx context.Context, req *pb.UserInfosRequest) (*pb.UserInfosReply, error) {
+	users, err := s.uc.GetInfos(context.TODO(), req.UserIds)
+	if err != nil {
+		return &pb.UserInfosReply{
+			StatusCode: 300,
+			StatusMsg:  err.Error(),
+		}, nil
+	}
+	reply := &pb.UserInfosReply{
+		StatusCode: 0,
+		StatusMsg:  "success",
+	}
+	reply.Users = make([]*pb.User, len(users))
+	for i, user := range users {
+		reply.Users[i] = &pb.User{
+			Id:              user.Id,
 			Name:            user.Name,
 			FollowCount:     user.FollowCount,
 			FollowerCount:   user.FollowerCount,
-			IsFollow:        user.IsFollow,
 			Avatar:          user.Avatar,
 			BackgroundImage: user.BackgroundImage,
 			Signature:       user.Signature,
 			TotalFavorited:  user.TotalFavorited,
 			WorkCount:       user.WorkCount,
 			FavoriteCount:   user.FavoriteCount,
-		},
+		}
+	}
+	return reply, nil
+}
+
+func (s *UserService) UpdateFollow(ctx context.Context, req *pb.UpdateFollowRequest) (*pb.UpdateFollowReply, error) {
+	err := s.uc.UpdateFollow(context.TODO(), req.UserId, req.FollowChange)
+	if err != nil {
+		return &pb.UpdateFollowReply{
+			StatusCode: 300,
+			StatusMsg:  err.Error(),
+		}, nil
+	}
+	return &pb.UpdateFollowReply{
+		UserId:     req.UserId,
+		StatusCode: 0,
+		StatusMsg:  "success",
+	}, nil
+}
+
+func (s *UserService) UpdateFollower(ctx context.Context, req *pb.UpdateFollowerRequest) (*pb.UpdateFollowerReply, error) {
+	err := s.uc.UpdateFollower(context.TODO(), req.UserId, req.FollowerChange)
+	if err != nil {
+		return &pb.UpdateFollowerReply{
+			StatusCode: 300,
+			StatusMsg:  err.Error(),
+		}, nil
+	}
+	return &pb.UpdateFollowerReply{
+		UserId:     req.UserId,
+		StatusCode: 0,
+		StatusMsg:  "success",
+	}, nil
+}
+
+func (s *UserService) UpdateFavorited(ctx context.Context, req *pb.UpdateFavoritedRequest) (*pb.UpdateFavoritedReply, error) {
+	err := s.uc.UpdateFavorited(context.TODO(), req.UserId, req.FavoritedChange)
+	if err != nil {
+		return &pb.UpdateFavoritedReply{
+			StatusCode: 300,
+			StatusMsg:  err.Error(),
+		}, nil
+	}
+	return &pb.UpdateFavoritedReply{
+		UserId:     req.UserId,
+		StatusCode: 0,
+		StatusMsg:  "success",
+	}, nil
+}
+
+func (s *UserService) UpdateWork(ctx context.Context, req *pb.UpdateWorkRequest) (*pb.UpdateWorkReply, error) {
+	err := s.uc.UpdateWork(context.TODO(), req.UserId, req.WorkChange)
+	if err != nil {
+		return &pb.UpdateWorkReply{
+			StatusCode: 300,
+			StatusMsg:  err.Error(),
+		}, nil
+	}
+	return &pb.UpdateWorkReply{
+		UserId:     req.UserId,
+		StatusCode: 0,
+		StatusMsg:  "success",
+	}, nil
+}
+
+func (s *UserService) UpdateFavorite(ctx context.Context, req *pb.UpdateFavoriteRequest) (*pb.UpdateFavoriteReply, error) {
+	err := s.uc.UpdateFavorite(context.TODO(), req.UserId, req.FavoriteChange)
+	if err != nil {
+		return &pb.UpdateFavoriteReply{
+			StatusCode: 300,
+			StatusMsg:  err.Error(),
+		}, nil
+	}
+	return &pb.UpdateFavoriteReply{
+		UserId:     req.UserId,
+		StatusCode: 0,
+		StatusMsg:  "success",
 	}, nil
 }
