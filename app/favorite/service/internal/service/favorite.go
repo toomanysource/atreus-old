0ad0c841f7c9d9e6cd371d0bcf1e0aa2aa4e7d8a
intyou <123916296+intyouss@users.noreply.github.com>
2023-08-08 15:09:35 +0800

    feat: Add and refine the favorite service method

--- app/favorite/service/internal/service/favorite.go
+++ app/favorite/service/internal/service/favorite.go
@@ -18,3 +18,51 @@ func NewFavoriteService(fu *biz.FavoriteUsecase, logger log.Logger) *FavoriteSer
 		log: log.NewHelper(log.With(logger, "model", "service/favorite")),
 	}
 }
+
+func (s *FavoriteService) GetFavoriteList(
+	ctx context.Context, req *pb.FavoriteListRequest) (*pb.FavoriteListReply, error) {
+	reply := &pb.FavoriteListReply{StatusCode: 0, StatusMsg: "success"}
+	videos, err := s.fu.GetFavoriteList(ctx, req.UserId, req.Token)
+	if err != nil {
+		reply.StatusCode = -1
+		reply.StatusMsg = err.Error()
+		return reply, nil
+	}
+	for _, video := range videos {
+		reply.VideoList = append(reply.VideoList, &pb.Video{
+			Id:    video.Id,
+			Title: video.Title,
+			Author: &pb.User{
+				Id:              video.Author.Id,
+				Name:            video.Author.Name,
+				FollowCount:     video.Author.FollowCount,
+				FollowerCount:   video.Author.FollowerCount,
+				IsFollow:        video.Author.IsFollow,
+				Avatar:          video.Author.Avatar,
+				BackgroundImage: video.Author.BackgroundImage,
+				Signature:       video.Author.Signature,
+				TotalFavorited:  video.Author.TotalFavorited,
+				WorkCount:       video.Author.WorkCount,
+				FavoriteCount:   video.Author.FavoriteCount,
+			},
+			PlayUrl:       video.PlayUrl,
+			CoverUrl:      video.CoverUrl,
+			FavoriteCount: video.FavoriteCount,
+			CommentCount:  video.CommentCount,
+			IsFavorite:    video.IsFavorite,
+		})
+	}
+	return reply, nil
+}
+
+func (s *FavoriteService) FavoriteAction(
+	ctx context.Context, req *pb.FavoriteActionRequest) (*pb.FavoriteActionReply, error) {
+	reply := &pb.FavoriteActionReply{StatusCode: 0, StatusMsg: "success"}
+	err := s.fu.FavoriteAction(ctx, req.VideoId, req.ActionType, req.Token)
+	if err != nil {
+		reply.StatusCode = -1
+		reply.StatusMsg = err.Error()
+		return reply, nil
+	}
+	return reply, nil
+}
