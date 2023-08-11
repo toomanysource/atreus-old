b6ce333e8e975499a3806b9a144fa69acec2c32d
Monet Lee <monet_lee@163.com>
2023-08-11 18:39:41 +0800

    feat: create feed cmd & config files

---
+++ app/feed/service/cmd/server/wire.go
@@ -0,0 +1,23 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"Atreus/app/feed/service/internal/biz"
+	"Atreus/app/feed/service/internal/conf"
+	"Atreus/app/feed/service/internal/data"
+	"Atreus/app/feed/service/internal/server"
+	"Atreus/app/feed/service/internal/service"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Client, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
