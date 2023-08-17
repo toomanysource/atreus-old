057fdbeb5f498473fe8c219d04eaace901c950f6
Monet Lee <monet_lee@163.com>
2023-08-17 17:10:47 +0800

    fix: fix rpc only send statCode

--- app/feed/service/internal/data/publish.go
+++ app/feed/service/internal/data/publish.go
@@ -5,6 +5,7 @@ import (
 	"Atreus/app/feed/service/internal/biz"
 	"Atreus/app/feed/service/internal/server"
 	"context"
+	"errors"
 
 	"github.com/jinzhu/copier"
 )
@@ -28,9 +29,9 @@ func (u *publishRepo) GetVideoList(
 	ctx context.Context, latestTime string, userId uint32, number uint32) (int64, []biz.Video, error) {
 
 	// call grpc function to fetch video list
-	resp, err := u.client.GetVideoList(ctx, &pb.VideoListRequest{LatestTime: latestTime, UserId: userId, Number: videoCount})
-	if err != nil {
-		return 0, nil, err
+	resp, _ := u.client.GetVideoList(ctx, &pb.VideoListRequest{LatestTime: latestTime, UserId: userId, Number: videoCount})
+	if resp.StatusCode != 0 {
+		return 0, nil, errors.New(resp.StatusMsg)
 	}
 
 	// convert pb.Video slice to biz.Video slice
